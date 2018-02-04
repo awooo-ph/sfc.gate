109deedcf301cb01264c5d1dc5ab33f57a812c2c
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-02-05 06:15:57 +0800

    message dialog

---
+++ SFC.Gate/ViewModels/MessageDialog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using MaterialDesignThemes.Wpf;
+
+namespace SFC.Gate.Material.ViewModels
+{
+    class MessageDialog 
+    {
+        public MessageDialog(string title, string message, PackIconKind icon, string acceptText) 
+            : this(title, message, icon, acceptText, false, null)
+        {}
+
+        public MessageDialog(string title, string message, PackIconKind icon, string acceptText, bool isCancellable, string cancelText)
+        {
+            Title = title;
+            Message = message;
+            Icon = icon;
+            AcceptText = acceptText;
+        
+            IsCancellable = isCancellable;
+            CancelText = cancelText;
+        
+        }
+
+        public PackIconKind Icon { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        
+        public string CancelText { get; set; }
+        public bool IsCancellable { get; set; }
+        
+        public string AcceptText { get; set; }
+        
+        
+    }
+}
