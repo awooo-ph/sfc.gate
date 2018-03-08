2b4e81e7c5741514bd7d144923f7fd6df803e54d
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-03-08 13:48:31 +0800

    v0.9.11

--- SFC.Gate/Properties/AssemblyInfo.cs
+++ SFC.Gate/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.10")]
-[assembly: AssemblyFileVersion("3")]
+[assembly: AssemblyVersion("0.9.11")]
+[assembly: AssemblyFileVersion("4")]
 [assembly: AssemblyInformationalVersion("Capstone 2018")]
